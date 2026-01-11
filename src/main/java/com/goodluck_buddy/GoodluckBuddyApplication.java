64d7da5c74bbb69508c8633a267bebd59efd266a
seoyoon lee <leeseoyoon01@gmail.com>
2026-01-11 18:57:04 +0900

    first commit

---
+++ src/main/java/com/goodluck_buddy/GoodluckBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.goodluck_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodluckBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodluckBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodluck_buddy/GoodluckBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodluck_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodluckBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
