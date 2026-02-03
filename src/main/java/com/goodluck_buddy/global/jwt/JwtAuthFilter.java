e62a770ee16ffd8cff6fe24b09e30f49f4e9356a
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-03 22:22:33 +0900

    feat: 토큰 발급 기능 구현 #4

---
+++ src/main/java/com/goodluck_buddy/global/jwt/JwtAuthFilter.java
@@ -0,0 +1,57 @@
+package com.goodluck_buddy.global.jwt;
+
+import com.goodluck_buddy.domain.user.entity.User;
+import com.goodluck_buddy.domain.user.exception.UserException;
+import com.goodluck_buddy.domain.user.exception.code.UserErrorCode;
+import com.goodluck_buddy.domain.user.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        // 토큰 가져오기
+        String token = request.getHeader("Authorization");
+        // token이 없거나 Bearer가 아니면 넘기기
+        if (token == null || !token.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // Bearer이면 추출
+        token = token.replace("Bearer ", "");
+        // AccessToken 검증하기: 올바른 토큰이면
+        if (jwtUtil.isValid(token)) {
+            // 토큰에서 이메일 추출
+            Long userId = Long.parseLong(jwtUtil.getId(token));
+            // 인증 객체 생성: 이메일로 찾아온 뒤, 인증 객체 생성
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UserException(UserErrorCode.NOT_FOUND));
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                    user,
+                    null,
+                    null
+            );
+            // 인증 완료 후 SecurityContextHolder에 넣기
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
