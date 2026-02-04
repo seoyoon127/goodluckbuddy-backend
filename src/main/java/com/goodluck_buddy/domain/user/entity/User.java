710b9f089cc6bb9693c6bb67e3dce95a91119946
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-04 16:35:42 +0900

    feat: kakao login 구현 #7

--- src/main/java/com/goodluck_buddy/domain/user/entity/User.java
+++ src/main/java/com/goodluck_buddy/domain/user/entity/User.java
@@ -28,21 +28,25 @@ public class User extends BaseEntity {
     @Column(name = "provider_id", unique = true, nullable = false)
     private String providerId;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "social_type", nullable = false)
     private SocialType socialType;
 
     @Column(name = "nickname")
     private String nickname;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "gender")
     private Gender gender;
 
     @Column(name = "birth")
     private LocalDate birth;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "interest_category")
     private Category interestCategory;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private Status status;
 }
