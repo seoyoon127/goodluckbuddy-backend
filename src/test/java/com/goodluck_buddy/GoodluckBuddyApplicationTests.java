482e878ea460826f12d932086b9daf85705ea5be
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-04 15:14:35 +0900

    test: CI에서 db 안 쓰도록 수정

--- src/test/java/com/goodluck_buddy/GoodluckBuddyApplicationTests.java
+++ src/test/java/com/goodluck_buddy/GoodluckBuddyApplicationTests.java
@@ -2,12 +2,14 @@ package com.goodluck_buddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GoodluckBuddyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
