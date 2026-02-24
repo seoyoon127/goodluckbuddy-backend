407be6bd29bacccf8d4037eebc222ddd0e8114b6
seoyoon lee <leeseoyoon01@gmail.com>
2026-02-24 21:25:49 +0900

    refactor: 좋아요한 편지 정렬 카테고리 수정

--- src/main/java/com/goodluck_buddy/domain/letter/service/LetterService.java
+++ src/main/java/com/goodluck_buddy/domain/letter/service/LetterService.java
@@ -89,7 +89,10 @@ public class LetterService {
             case LATEST -> Sort.by(Sort.Direction.DESC, "createdAt");
             case LIKE -> Sort.by(Sort.Direction.DESC, "likeCount");
         };
-        List<Letter> letters = letterRepository.findAllByFiltersWithLike(category, id, sort);
+
+        Category filterCategory =
+                (category == Category.ALL) ? null : category;
+        List<Letter> letters = letterRepository.findAllByFiltersWithLike(filterCategory, id, sort);
         return letters.stream()
                 .map(letter -> {
                     User writer = userRepository.findById(letter.getWriterId())
